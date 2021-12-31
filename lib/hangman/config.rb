9fa2fa53f0de320662ac623dab6720c98bcff2b5
Uriel Juliatti Valle <uriel.juliattivalle@gmail.com>
2021-12-31 16:37:58 -0300

    rubocop fix

--- lib/hangman/config.rb
+++ lib/hangman/config.rb
@@ -5,7 +5,7 @@ require 'i18n'
 module Hangman
   # Responsável por carregar as informações iniciais do jogo
   module Config
-    I18n.load_path << Dir[File.expand_path('config/locales') + '/*.yml']
+    I18n.load_path << Dir["#{File.expand_path('config/locales')}/*.yml"]
     I18n.default_locale = :'pt-BR'
 
     def self.build
