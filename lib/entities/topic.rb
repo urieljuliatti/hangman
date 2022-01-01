631c6b0dedf84858aa08760e46d438e7c4336406
Uriel Juliatti Valle <uriel.juliattivalle@gmail.com>
2022-01-01 13:37:23 -0300

    fix para tradução de conjunção

--- lib/entities/topic.rb
+++ lib/entities/topic.rb
@@ -3,7 +3,7 @@
 module Entities
   Topic = Struct.new :subject, :keyword, :kind, :name, keyword_init: true do
     def display
-      "[#{I18n.t('topic.subject')}]: #{subject} and [#{I18n.t('topic.kind')}]: #{kind}".color(:yellow)
+      "[#{I18n.t('topic.subject')}]: #{subject} #{I18n.t('grammar.and')} [#{I18n.t('topic.kind')}]: #{kind}".color(:yellow)
     end
   end
 end
