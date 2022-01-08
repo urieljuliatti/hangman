6e1d8f546fd4106c631ba3f884819ca590d4c115
Uriel Juliatti Valle <uriel.juliattivalle@gmail.com>
2022-01-08 16:30:12 -0300

    Ajustando o load path do I18n

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -2,6 +2,9 @@
 require 'i18n'
 require 'pry'
 
+I18n.load_path << Dir["#{File.expand_path('config/locales')}/*.yml"]
+I18n.default_locale = ARGV.empty? ? :en : ARGV[0].to_sym
+
 require './lib/hangman/config'
 require './lib/entities/keywordable'
 require './lib/ui/emoji'
@@ -12,6 +15,3 @@ require './lib/repositories/topic'
 require './lib/repositories/repo_file'
 require './lib/entities/topic'
 require './lib/ui/text_color'
-
-I18n.load_path << Dir["#{File.expand_path('config/locales')}/*.yml"]
-I18n.default_locale = ARGV.empty? ? :en : ARGV[0].to_sym
